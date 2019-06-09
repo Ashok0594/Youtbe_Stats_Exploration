4d85ee1e1d896971914f54bab898b29a1d64e218
Ashok Ramasami <mischievousmechanic@hotmail.com>
2019-06-09 10:39:37 +0530

    Initital Commit

---
+++ data_prep.py
@@ -0,0 +1,11 @@
+#####################################################################
+# Created by : Team Pixel                                           #
+# Created on : 26-Oct-2018                                          #
+# Program Name : data_prep                                          #
+# Description : Data Changes to the source data                     #
+#####################################################################
+
+import csv
+
+with open('USvideos.csv') as csv_file:
+
